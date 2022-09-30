483610956f74b0346bf283fc765db7337101a562
Himapandey <114731376+Himapandey@users.noreply.github.com>
2022-09-30 13:54:29 +0530

    Add files via upload
    Assignment 1

---
+++ SoftwareEngineer.java
@@ -0,0 +1,24 @@
+class SoftwareEngineer{
+   public static void main(String... args){
+
+   int exp = 5;
+    if(exp <=2){
+         System.out.println("Associate Engineer");
+} else if(exp > 2 && exp <= 5) {
+        System.out.println("Software Engineer");
+}
+else if(exp>5 && exp<=7){
+
+  System.out.println("Sr. Software Engineer");
+}
+
+else if(exp>7 && exp<=10){
+  System.out.println("Sr. Software Engineer");
+}
+     
+ 
+  }
+
+
+
+}
